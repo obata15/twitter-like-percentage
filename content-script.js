4f20207658b3dfab88584555452006c83f4a41ac
Shinsuke Obata <studio15.jp@gmail.com>
2022-12-24 13:40:20 +0900

    Impl

---
+++ content-script.js
@@ -0,0 +1,30 @@
+function replaceViewCount (article) {
+  const analytics = article.querySelector('a[href$="analytics"]');
+  const viewCount = analytics.ariaLabel?.match(/^\d+/)?.[0];
+
+  const like = article.querySelector('[data-testid="like"]');
+  const likeCount = like.ariaLabel?.match(/^\d+/)?.[0];
+
+  const ppm = Math.round(likeCount * 10000 / viewCount);
+
+  const analyticsValueSpan = analytics.querySelector("div > div > span > span");
+
+  if (!isNaN(ppm)) {
+    analyticsValueSpan.innerText =  ppm + " ppm";
+  }
+}
+
+const mutationObserver = new MutationObserver((mutationRecords) => {
+  mutationRecords.forEach((mutationRecord) => {
+    const articles = mutationRecord.target.querySelectorAll('article');
+
+    for (let article of articles) {
+      replaceViewCount(article);
+    }
+  })
+});
+
+mutationObserver.observe(
+  document.body,
+  { childList: true, subtree: true }
+);
